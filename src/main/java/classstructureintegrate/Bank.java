a3e0cd7736b09c27e78e7fc71a597f945f76729a
anessza <42357124+anessza@users.noreply.github.com>
2020-11-08 00:14:03 +0100

    Bank

---
+++ src/main/java/classstructureintegrate/Bank.java
@@ -0,0 +1,43 @@
+package classstructureintegrate;
+
+import java.util.Scanner;
+
+public class Bank {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("First bankAccount:");
+        String account1= sc.nextLine();
+        System.out.println("First owner:");
+        String owner1= sc.nextLine();
+        System.out.println("First balance:");
+        int balance1= sc.nextInt();
+
+        sc.nextLine();
+
+        System.out.println("Second bankAccount:");
+        String account2= sc.nextLine();
+        System.out.println("Second owner:");
+        String owner2= sc.nextLine();
+        System.out.println("Second balance:");
+        int balance2= sc.nextInt();
+
+        BankAccount firstAccount = new BankAccount(account1, owner1, balance1);
+        BankAccount secondAccount = new BankAccount(account2, owner2, balance2);
+
+        System.out.println(firstAccount.getInfo());
+        System.out.println(secondAccount.getInfo());
+
+        System.out.println(firstAccount.getOwner() + " deposit (Ft):");
+        firstAccount.deposit(sc.nextInt());
+        System.out.println(firstAccount.getOwner() + " transfer to " + secondAccount.getOwner() + " (Ft):");
+        int transfer = firstAccount.transfer(sc.nextInt());
+        firstAccount.withdraw(firstAccount.transfer(transfer));
+        secondAccount.deposit(secondAccount.transfer(transfer));
+
+        System.out.println(firstAccount.getInfo());
+        System.out.println(secondAccount.getInfo());
+
+
+    }
+
+}
---
+++ src/main/java/classstructureintegrate/BankAccount.java
@@ -0,0 +1,49 @@
+package classstructureintegrate;
+
+public class BankAccount {
+    private String accountNumber;
+    private String owner;
+    private int balance;
+
+    public BankAccount(String accountNumber, String owner, int balance) {
+        this.accountNumber = accountNumber;
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+    public void deposit(int deposit) {
+        this.balance = balance + deposit;
+    }
+    public void withdraw(int withdraw) {
+        this.balance = balance - withdraw;
+    }
+    public int transfer(int transfer) {
+        return transfer;
+    }
+    public String getInfo() {
+        return owner  + " (" + accountNumber + "): " + balance + " Ft";
+    }
+}
