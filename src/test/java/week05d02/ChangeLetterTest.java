8a162d59fe0dc1750d79e15dff859973d43d489a
anessza <42357124+anessza@users.noreply.github.com>
2020-11-27 23:56:04 +0100

    ChangeLetter TEST

---
+++ src/test/java/week05d02/ChangeLetterTest.java
@@ -0,0 +1,18 @@
+package week05d02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static week05d02.ChangeLetter.changeVowels;
+
+
+public class ChangeLetterTest {
+
+        @Test
+       void testChangeLetter(){
+            String test = "This program change letters OUM";
+            String solved= changeVowels(test);
+            assertEquals   ("Th*s pr*gr*m ch*ng* l*tt*rs **M", solved );
+    }
+
+}
