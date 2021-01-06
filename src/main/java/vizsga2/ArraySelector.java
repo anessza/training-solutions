2fc5a09130c2fb8d4ba85144806b4200bb467870
anessza <42357124+anessza@users.noreply.github.com>
2021-01-06 20:40:02 +0100

    Vizsga2: Tömbök

--- src/main/java/vizsga2/ArraySelector.java
+++ src/main/java/vizsga2/ArraySelector.java
@@ -1,5 +1,40 @@
 package vizsga2;
+/*
+Írj egy olyan metódust az ArraySelector osztályban, mely egy kapott tömbből minden második
+elemet kiválogat, és egy szövegként összerak, szögletes zárójelek között!
+A páros megállapítása index szerint működik. Azaz az első, nulla indexű
+elem páros, a második, azaz egyes indexű elem páratlan, stb.
+Ha nulla elemű a tömb, akkor üres stringet adjon vissza!
+ */
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ArraySelector {
+    static String arraySelector(String [] array) {
+        if (array.length == 0) return null;
+        else {
+            List<String> newStringList = new ArrayList<>();
+            for (int i=0; i< array.length;i++) {
+                if ((i % 2) == 0) {
+                    newStringList.add(array[i]);
+                }
+            }
+            String newString = newStringList.toString();
+            return newString.replace(", ", "");
+        }
+    }
 
+
+    public static void main(String[] args) {
+
+        String[] test= {"Alma","Körte","Dinnye","Paprika","Paradicsom","Répa", "Banán"};
+
+        String newList= arraySelector(test);
+        System.out.println(newList);
+
+    }
 }
+
+
+
